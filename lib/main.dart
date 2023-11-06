2f655844544e71d706ef184098d1d1f4107f4166
Krish Porwal <101163531+krishporwal14@users.noreply.github.com>
2023-11-06 19:59:54 +0530

    Day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import './home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text("Welcome to 30 days of flutter"),
-        ),
-      ),
-    );
+    return HomePage();
   }
 }
