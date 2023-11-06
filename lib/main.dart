c1173d450f358515ed919a908d4b36e632814ec3
Krish Porwal <101163531+krishporwal14@users.noreply.github.com>
2023-11-06 20:41:46 +0530

    Day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import './pages/home_page.dart';
+import './pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
